548693d26fd02379c820ba1968557c3b8e579bef
임하나 <hanadul1@douzone.com>
2021-07-19 13:31:00 +0900

    클로저 이해하기

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-let i, len = 3;
-for(i = 0; i < len; i++) {
-    document.getElementById("divScope" + i).addEventListener(
-        "click",
-        (function (index) {
-            return function () {
-                alert("You clicked div #" + index);
-            };
-        }(i)),
-    false);
+function outer() {
+    let count = 0;
+    let inner = function() {
+        return ++count;
+    };
+    return inner;
 }
+let increase = outer();
+
+console.log(increase());
+console.log(increase());
